b3a506dd1db7664bdfe24d1ce1805e598b3b505c
ghaa2023 <144506214+ghaa2023@users.noreply.github.com>
2023-10-27 21:04:11 +0700

    Update youtube desa

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -15,7 +15,7 @@
       <iframe
         width="480"
         height="260"
-        src="https://www.youtube.com/embed/X8PyS6qav1s"
+        src="https://youtu.be/BkMcs4bwbOk?si=27W1qJ17HNnIo75P"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
